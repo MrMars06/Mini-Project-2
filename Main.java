4d8f11b9338f75c9548c1ea1d4c7883b7bf4987a
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-10-16 04:41:56 +0000

    I added a comment for the loop, and added the loop statement, but does not appear to work. Am fixing.

--- Main.java
+++ Main.java
@@ -6,13 +6,18 @@ class Main {
     // This adds the Scanner to the code
     Scanner scan = new Scanner(System.in);
 
+    // This for loop will print a statement 3 times about which side you will fight for
+    for (int i = 0; < 3; i++) {
+      System.out.println("The Council will decide your fate. The 9 year old army, or the 19 year old army. You have no choice.");
+    }
+
     // The code below asks you what my name and age is using Scanner
     System.out.println("What's your first name?");
     String name = scan.next();
 
     System.out.println("What's your age?");
     int age = scan.nextInt();
-    
+
     // The code below is the if else statement that appear after you enter your age with Scanner
     if(age == 9) {
       System.out.println("Congratulations! You are officially part of the nine year old army!");
@@ -20,5 +25,6 @@ class Main {
     } else if(age > 9) {
         System.out.println("Wow, you are automatically part of the 19 year old army! Congratulations!");
     }
+    
   }
 }
