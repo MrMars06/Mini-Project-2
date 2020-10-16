185aa4ca4ca6f46de8deb4d466941e39ccebdcb3
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-10-16 04:20:33 +0000

    I accidentally forgot to commit until now. I added a scanner, and 1 if else statement.

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What's your first name?");
+    String name = scan.next();
+
+    System.out.println("What's your age?");
+    int age = scan.nextInt();
+
+    if(age == 9) {
+      System.out.println("Congratulations! You are officially part of the nine year old army!");
+
+    } else if(age > 9) {
+        System.out.println("Wow, you are automatically part of the 19 year old army! Congratulations!");
+    }
   }
 }
