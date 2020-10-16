e15a67a0b0451597ec05c7f3a9f7c1cbd789ead2
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-10-16 04:23:46 +0000

    I added some comments to my code.

--- Main.java
+++ Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     
+    // This adds the Scanner to the code
     Scanner scan = new Scanner(System.in);
-
+    // The code below asks you what my name and age is using Scanner
     System.out.println("What's your first name?");
     String name = scan.next();
 
     System.out.println("What's your age?");
     int age = scan.nextInt();
-
+    // The code below is the if else statement that appear after you enter your age with Scanner
     if(age == 9) {
       System.out.println("Congratulations! You are officially part of the nine year old army!");
 
