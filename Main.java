655ab483e69f06c13645d18917822b09d3926811
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-10-16 04:26:30 +0000

    I changed the name and description of the repl.

--- Main.java
+++ Main.java
@@ -5,12 +5,14 @@ class Main {
     
     // This adds the Scanner to the code
     Scanner scan = new Scanner(System.in);
+
     // The code below asks you what my name and age is using Scanner
     System.out.println("What's your first name?");
     String name = scan.next();
 
     System.out.println("What's your age?");
     int age = scan.nextInt();
+    
     // The code below is the if else statement that appear after you enter your age with Scanner
     if(age == 9) {
       System.out.println("Congratulations! You are officially part of the nine year old army!");
