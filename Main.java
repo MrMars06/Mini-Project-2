9818fdb7b2d9536b96f77bc6854cd3916b9f41fd
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-10-16 04:45:11 +0000

    I moved the for Loop to the end of the code.

--- Main.java
+++ Main.java
@@ -6,10 +6,6 @@ class Main {
     // This adds the Scanner to the code
     Scanner scan = new Scanner(System.in);
 
-    // This for loop will print a statement 3 times about which side you will fight for
-    for (int i = 0; < 3; i++) {
-      System.out.println("The Council will decide your fate.");
-    }
 
     // The code below asks you what my name and age is using Scanner
     System.out.println("What's your first name?");
@@ -25,6 +21,10 @@ class Main {
     } else if(age > 9) {
         System.out.println("Wow, you are automatically part of the 19 year old army! Congratulations!");
     }
-    
+
+    // This for loop that will print a statement 3 times about which side you will fight for
+    for (int i = 0; < 3; i++) {
+      System.out.println("The Council has decided your fate.");
+    }
   }
 }
